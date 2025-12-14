93acbde79ac607f61a8ee4d25fda4047a249fc43
Shivaguru Pachamuthu <sgkodi2007@gmail.com>
2025-12-14 22:46:53 +0530

    fix: minor issues in image

--- src/components/communityHeader.tsx
+++ src/components/communityHeader.tsx
@@ -1,6 +1,4 @@
 import { motion } from 'framer-motion';
-import logo from '../assets/f01b45f7-eb29-4c78-8ddb-e21869cba54c_removalai_preview copy.png'
-import image from '../assets/market.png'
 import sidebar from '../assets/sidebar menu.png';
 import ZigzagLine from './zigzagLine';
 //import DiagonalLinesComponent from './diagonalLine';    
@@ -26,9 +24,6 @@ const CommunityHeader = ({ details, title }: CommunityHeaderProps) => {
         viewport={{ once: true, amount: 0.5 }} // `once: true` ensures it only triggers once
         transition={{ duration: 0.5, ease: "easeOut" }}
         className='flex justify-between items-center fixed w-full lg:pr-28 pr-16 md:hidden'>
-            <div onClick={() => handleClick("/")} className='w-16 cursor-pointer'>
-                <img src={logo} alt="Logo" />
-            </div>
             <div onClick={() => handleClick("/menu")} className='w-16 hover:bg-[rgb(204,165,76)] rounded-[5px] duration-150 ease-out cursor-pointer'>
                 <img src={sidebar} alt="Logo" />
             </div>
@@ -51,9 +46,7 @@ const CommunityHeader = ({ details, title }: CommunityHeaderProps) => {
             <div className='w-full heading-text'>
                 <div className='lg:text-[200px] text-6xl'>{title}</div>
             </div>
-            <div className='md:hidden flex justify-center'>
-                <div><img className='w-full h-full object-center' src={image} alt="" /></div>
-            </div>
+
         </motion.div>
 
 
